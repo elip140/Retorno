4fa2ec0ea20235990c2f7682a100e58cfb6b4c5e
elip140 <epcotrim@gmail.com>
2022-01-06 17:29:44 -0300

    http request

--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Net;
 
 namespace Retorno.Pages;
 
@@ -30,14 +31,27 @@ public class IndexModel : PageModel
         //response = await client.GetStringAsync("http://187.87.242.13:8084/cgi-bin/recordFinder.cgi?action=find&name=AccessControlCardRec&StartTime=1640995200&EndTime=1643673599");
         
 
-        Usu = Request.Form["Usuario"];
+        /*Usu = Request.Form["Usuario"];
         Pass = Request.Form["Senha"];
         var formContent = new FormUrlEncodedContent(new[]
         {
             new KeyValuePair<string, string>("userName", Request.Form["Usuario"]),
             new KeyValuePair<string, string>("password", Request.Form["Senha"]),
-        });
-        return Page();
+        });*/
+
+    const string userName = @"admin";
+    const string password = @"admin@01";
+    string domain = "http://187.87.242.13:8084/";
+
+    var credCache = new CredentialCache();
+    credCache.Add(new Uri(domain), "Digest", new NetworkCredential(userName, password));
+
+    var httpNClient = new HttpClient(new HttpClientHandler { Credentials = credCache });
+    var answer = await httpNClient.GetAsync(new Uri($"{domain}cgi-bin/recordFinder.cgi?action=find&name=AccessControlCardRec&StartTime=1640995200&EndTime=1643673599"));
+    response = answer.Content.ReadAsStringAsync().Result;
+
+    
+    return Page();
     }
 
     /*
