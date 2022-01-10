b92ccc75bdbdb8a268eab4a3e61d1de632a263c9
elip140 <epcotrim@gmail.com>
2022-01-10 15:37:21 -0300

    Get String

---
+++ Models/ControlCard.cs
@@ -0,0 +1,15 @@
+
+namespace Retorno.Models
+{
+    public class ControlCard
+    {
+        public int AccessControlCardRecID;
+        public String AttendanceState;
+        public String CardName;
+        public String CardNo;
+        public String CardType;
+
+
+
+    }
+}
--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Net;
+using Newtonsoft.Json;
 
 namespace Retorno.Pages;
 
@@ -28,6 +29,25 @@ public class IndexModel : PageModel
     }
     public async Task<IActionResult> OnPostAsync()
     {
+        const string userName = @"admin";
+        const string password = @"admin@01";
+        string domain = "http://187.87.242.13:8084/";
+
+        var credCache = new CredentialCache();
+        credCache.Add(new Uri(domain), "Digest", new NetworkCredential(userName, password));
+
+        var httpNClient = new HttpClient(new HttpClientHandler { Credentials = credCache });
+        var answer = await httpNClient.GetAsync(new Uri($"{domain}cgi-bin/recordFinder.cgi?action=find&name=AccessControlCardRec&StartTime=1640995200&EndTime=1643673599"));
+        var res = answer.Content.ReadAsStringAsync().Result;
+
+        Console.WriteLine(res);
+    
+        return Page();
+    }
+}
+
+
+
         //response = await client.GetStringAsync("http://187.87.242.13:8084/cgi-bin/recordFinder.cgi?action=find&name=AccessControlCardRec&StartTime=1640995200&EndTime=1643673599");
         
 
@@ -38,22 +58,6 @@ public class IndexModel : PageModel
             new KeyValuePair<string, string>("userName", Request.Form["Usuario"]),
             new KeyValuePair<string, string>("password", Request.Form["Senha"]),
         });*/
-
-    const string userName = @"admin";
-    const string password = @"admin@01";
-    string domain = "http://187.87.242.13:8084/";
-
-    var credCache = new CredentialCache();
-    credCache.Add(new Uri(domain), "Digest", new NetworkCredential(userName, password));
-
-    var httpNClient = new HttpClient(new HttpClientHandler { Credentials = credCache });
-    var answer = await httpNClient.GetAsync(new Uri($"{domain}cgi-bin/recordFinder.cgi?action=find&name=AccessControlCardRec&StartTime=1640995200&EndTime=1643673599"));
-    response = answer.Content.ReadAsStringAsync().Result;
-
-    
-    return Page();
-    }
-
     /*
     const string userName = @"admin";
     const string password = @"admin@01";
@@ -66,4 +70,3 @@ public class IndexModel : PageModel
     var answer = await httpNClient.GetAsync(new Uri($"{domain}cgi-bin/recordFinder.cgi?action=find&name=AccessControlCardRec&StartTime=1640995200&EndTime=1643673599"));
     var allaccess = answer.Content.ReadAsStringAsync().Result
     */
-}
